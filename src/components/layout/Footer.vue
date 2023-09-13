93de87b7c55d4daf76f3b720a0d6bd1e26bf2871
Myat-Theingi-Aung <myattheingiaung1720@gmail.com>
2023-09-13 09:02:22 +0630

    add layout and not found page

---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container-fluid bg-light">
+    <div class="row">
+      <div class="col-12">
+        <div class="d-flex py-3 align-items-center justify-content-between">
+          <h5 class="mb-0">Myat Theingi Aung</h5>
+          <p class="mb-0">Copyright&copy; MTA Co., Ltd. All rights reserved</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h1>Page Not Found</h1>
+      </div>
+    </div>
+  </div>
+</template>
